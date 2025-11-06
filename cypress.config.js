29e1c90db2762b76b08d5dcd4f130f322430309e
Leonard-Coneglian <leonardo.coneglian@cotefacil.com>
2025-11-06 18:00:05 -0300

    correcao  cypress..config

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
-  projectId: "6ebiru",
+  projectId: '6ebiru',
   e2e: {
     // setupNodeEvents(on, config) {
     //   // implement node event listeners here
